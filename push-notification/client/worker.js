ec2690c69c251bd5d2bbad2eff6da18f26f9b31e
Stepan <stepan.naryshkov@gmail.com>
2024-06-01 20:07:31 +0300

    Delete push-notification directory

--- push-notification/client/worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('push', event => { // Listen for push events
-    const data = event.data.json(); // Parse the incoming data as JSON
-    self.registration.showNotification(data.title, { // Show a notification with the parsed data
-      body: data.body // Set the body of the notification to the body field from the data
-    });
-  });
-  
